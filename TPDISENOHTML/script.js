b74daa045e24752f09d023c6d2d14536fa371b75
MateoSantar <136135064+MateoSantar@users.noreply.github.com>
2025-04-28 20:09:24 -0300

    scales changed in the js and some minor changes in style

--- TPDISENOHTML/script.js
+++ TPDISENOHTML/script.js
@@ -11,9 +11,9 @@ document.addEventListener('mousemove', (e) => {
   const y = e.clientY - centerY;
 
   // Mover el iris (como antes)
-  const maxMove = 300;
-  const distanceX = Math.max(-maxMove, Math.min(maxMove, x));
-  const distanceY = Math.max(-maxMove, Math.min(maxMove, y));
+  const maxMove = 250;
+  const distanceX = Math.max(-maxMove, Math.min(maxMove, x/2));
+  const distanceY = Math.max(-maxMove, Math.min(maxMove, y/2));
   iris.style.left = `calc(50% + ${distanceX}px)`;
   iris.style.top = `calc(50% + ${distanceY}px)`;
 
@@ -22,8 +22,8 @@ document.addEventListener('mousemove', (e) => {
 
   // Cambiar tamaño de la pupila según distancia
   if (distance < 200) {
-    pupila.style.transform = "translate(-50%, -50%) scale(1.5)";
-    iris.style.transform = "translate(-50%, -50%) scale(1.6)";
+    pupila.style.transform = "translate(-50%, -50%) scale(1.2)";
+    iris.style.transform = "translate(-50%, -50%) scale(1.2)";
     ojo.style.borderRadius = "100% 0%"
   } else {
     pupila.style.transform = "translate(-50%, -50%) scale(1)";
@@ -33,24 +33,21 @@ document.addEventListener('mousemove', (e) => {
 });
 
 function parpadear() {
-  // Cierra el ojo
   ojo.classList.add('cerrado');
 
-  // Abre el ojo después de 200ms (duración del parpadeo)
   setTimeout(() => {
     ojo.classList.remove('cerrado');
-  }, 200);
+  }, 300);
 }
 
-// Parpadea de vez en cuando (cada 3 a 6 segundos, aleatoriamente)
 function iniciarParpadeo() {
   const intervalo = Math.random() * 3000 + 3000; // entre 3000 y 6000 ms
   setTimeout(() => {
     parpadear();
-    iniciarParpadeo(); // llama recursivamente para seguir parpadeando
+    iniciarParpadeo();
   }, intervalo);
 }
 setTimeout(() => {
   iniciarParpadeo();
-}, 2000);
+}, 3000);
 
